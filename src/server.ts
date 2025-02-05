42c1b8e7c73c47f33445d8edbbbf6b82701f6d5a
Franco Caceres <francoal166177@gmail.com>
2025-02-05 02:22:04 -0300

    refactorizacion de server.ts en conjunto con router.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import router from './router';
 import sequelize from './config/db';
-import { circuitBreakerMiddleware } from './middleware/circuitBreaker'; //esto se va al router.ts
 import { rateLimiter } from './middleware/rateLimiter';
 
 const server = express();
@@ -15,9 +14,7 @@ server.use(express.urlencoded({ extended: true }));
 // Middleware de limitación de tasa
 server.use(rateLimiter);
 
-// Middleware de circuito de interrupción
-server.use(circuitBreakerMiddleware);// borrar
-
+// Rutas del API de pagos
 server.use('/api/payment', router);
 
 // Conectar a la base de datos

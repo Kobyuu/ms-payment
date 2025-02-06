a43cf1ae833fa133ac33c807cdf6d5dd3da35b25
Franco Caceres <francoal166177@gmail.com>
2025-02-06 01:23:42 -0300

    correccion en el archivo de environment.ts

--- src/config/constants/environment.ts
+++ src/config/constants/environment.ts
@@ -2,14 +2,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const config = {
-  databaseUrl: process.env.DATABASE_URL!,
-  inventoryServiceUrl: process.env.INVENTORY_SERVICE_URL!,
-  productServiceUrl: process.env.PRODUCT_SERVICE_URL!,
-  redisUrl: process.env.REDIS_URL!,
-  retryCount: parseInt(process.env.RETRY_COUNT!, 10),
-  retryDelay: parseInt(process.env.RETRY_DELAY!, 10),
-  outputStock: parseInt(process.env.OUTPUT_STOCK!, 10),
-  cacheExpiry: parseInt(process.env.CACHE_EXPIRY!, 10),
-  port: parseInt(process.env.PORT!, 10),
+export const CONFIG = {
+  DATABASE_URL: process.env.DATABASE_URL!,
+  INVENTORY_SERVICE_URL: process.env.INVENTORY_SERVICE_URL!,
+  PRODUCT_SERVICE_URL: process.env.PRODUCT_SERVICE_URL!,
+  REDIS_URL: process.env.REDIS_URL!,
+  RETRY_COUNT: parseInt(process.env.RETRY_COUNT!, 10),
+  RETRY_DELAY: parseInt(process.env.RETRY_DELAY!, 10),
+  OUTPUT_STOCK: parseInt(process.env.OUTPUT_STOCK!, 10),
+  CACHE_EXPIRY: parseInt(process.env.CACHE_EXPIRY!, 10),
+  PORT: parseInt(process.env.PORT!, 10),
 };

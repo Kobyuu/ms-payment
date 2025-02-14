5c35e0f84ee610c894a9a0be06a31eb10eac88c1
mauurom <idouublem3@gmail.com>
2025-02-13 22:00:40 -0300

    Limite de solicitudes aumentado

--- src/middleware/rateLimiter.ts
+++ src/middleware/rateLimiter.ts
@@ -3,7 +3,7 @@ import { ERROR_MESSAGES } from '../config/constants/messages';
 
 export const rateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutos
-  max: 100, // Limita cada IP a 100 solicitudes por ventana de tiempo
+  max: 100000, // Limita cada IP a 100 solicitudes por ventana de tiempo
   message: {
     status: 429,
     message: ERROR_MESSAGES.RATE_LIMITER.TOO_MANY_REQUESTS,
